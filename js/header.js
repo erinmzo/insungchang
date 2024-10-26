04d244d5feaed00caa32f05a19e4421b63234bde
Minsoo Cho <textign@gmail.com>
2024-10-26 16:19:17 +0900

    .

---
+++ js/header.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+  // 햄버거 메뉴
+  $('#header #main_menu_box > .inner .mobileBtn').click(function(){
+   
+    $('#header').toggleClass('on');    
+    $('#header .mainMenu').toggle();
+
+  });
+
+  
+});
+
